e0dcdcf5d39f2324be2b6c1193488cddc8615123
Mwasambu <81552851+Mwasambu@users.noreply.github.com>
2021-04-01 14:03:43 +0300

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+#Enter first and last name
+names = input("Enter first and last name")
+print(names)
+print()
+#Enter your goals for this class
+print("What are your goals for this class - long term goals?")
+goals = input('What would you like to be....when all is said and done with the class?')
+print(goals)
